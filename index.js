79424bfc53a4d5e0c06c5a21f4ede70334915198
IulianaJ <64654877+IulianaJ@users.noreply.github.com>
2020-09-03 14:40:09 +0300

    fixed positions linked button products with products fixed styling
    fixed positions
    linked button products with products
    fixed styling

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 var allObjects = [
     {
-        image: "../Scarlet/img/4cb9045ab276e8b9b129cd5e327a4ea1.png",
+        image: "./img/4cb9045ab276e8b9b129cd5e327a4ea1.png",
         title: "Scarlet Duet",
+        name: "scarlet"
     },
 
     {
-        image: "../Scarlet/img/34383-5-silver-ring-photos.png",
+        image: "./img/34383-5-silver-ring-photos.png",
         title: "Calyope",
+        name: "calyope"
     },
 
     {
-        image: "../Scarlet/img/Image_1.png",
+        image: "./img/Image_1.png",
         title: "Calipso",
+        name: "calipso"
     },
 
     {
-        image: "../Scarlet/img/Img_2.png",
+        image: "./img/Img_2.png",
         title: "Fortuna",
+        name: "fortuna"
     },
 
     {
-        image: "../Scarlet/img/Img_4.png",
+        image: "./img/Img_4.png",
         title: "Afrodite",
+        name: "afrodite"
     },
     {
-        image: "../Scarlet/img/Img_5.png",
+        image: "./img/Img_5.png",
         title: "Dyonis",
+        name: "dyonis"
     },
 
 ];
@@ -47,9 +53,10 @@ function showCard(obj, ID) {
     card.appendChild(image);
     card.appendChild(hash);
     document.getElementById(ID).appendChild(card);
-    var button = document.createElement("button");
+    var button = document.createElement("a");
     card.appendChild(button);
     button.setAttribute("class", "button3");
+    button.setAttribute("href", `${obj.name}.html`)
     button.innerText = "View product";
 }
 
--- signin.js
+++ signin.js
@@ -10,39 +10,44 @@ form.appendChild(alert);
 var validateEmail = false;
 var validatePassword = false;
 
+function showErr(field, msg) {
+    field.setAttribute("style", "border:3px solid rgb(139 129 118)");
+    alert.innerText = msg;
+    form.appendChild(alert);
+}
+
+function verifyEmail() {
+    var emailLetter = userEmail.value.split("");
+    emailLetter.forEach(function (item) {
+        if (item === "@") {
+            validateEmail = true;
+        }
+    });
+    if (validateEmail === false) {
+        showErr(userEmail, "Please insert a valid email address");
+    } else {
+        userEmail.setAttribute("style", "border:3px solid rgb(139 129 118)");
+    }
+}
+
+function verifyPassword() {
+    var passwordLetter = userPassword.value;
+    if (passwordLetter.length > 8) {
+        validatePassword = true;
+    }
+    if (validatePassword === false) {
+        showErr(userPassword, "The password is too short");
+    }
+}
+
 function verifyUser(event) {
-    function verifyEmail() {
-        var emailLetter = userEmail.value.split("");
-        emailLetter.forEach(function (item) {
-            if (item === "@") {
-                validateEmail = true;
-            }
-        });
-        if (validateEmail === false) {
-            userEmail.setAttribute("style", "border:3px solid rgb(139 129 118)");
-            alert.innerText = "Please insert a valid email address";
-            form.appendChild(alert);
-        } else {
-            userEmail.setAttribute("style", "border:3px solid rgb(139 129 118)");
-        }
-    }
-    function verifyPassword() {
-        var passwordLetter = userPassword.value;
-        if (passwordLetter.length > 8) {
-            validatePassword = true;
-        }
-        if (validatePassword === false) {
-            userPassword.setAttribute("style", "border:3px solid rgb(139 129 118)");
-            alert.innerText = "The password is too short";
-            form.appendChild(alert);
-        }
-    }
     verifyPassword();
     verifyEmail();
-    if (validateEmail === false || validatePassword === false) {
+
+    if (!validateEmail || !validatePassword) {
         event.preventDefault();
     } else {
-        window.location.replace("../Scarlet/index.html");
+        window.location.replace("./index.html");
     }
 }
 logIn.addEventListener("click", verifyUser);
